71fc435be1a3348322d482010ef0bbfed5b45874
Harald Schafer <harald.the.engineer@gmail.com>
2020-04-16 22:09:00 -0700

    well I misunderstoof that

---
+++ download_data_v2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import requests
+import os.path
+
+
+BASE_DIR = '/raid.dell1/world/'
+MAX_ZOOM = 10
+
+
+def save_img(req, save_path):
+  image_url = 'http://ecn.t1.tiles.virtualearth.net/tiles/a' + req + '.jpeg?g=8384'
+
+  receive = requests.get(image_url)
+  with open(save_path,'wb') as f:
+    f.write(receive.content)
+
+
+
+def get_quarters(base_dir, base_req, depth):
+  for q in range(4):
+    if not os.path.isdir(base_dir + str(q)):
+      os.mkdir(base_dir + str(q))
+      save_img(base_req + str(q), base_dir + str(q) + '/img.jpeg')
+    print(base_req)
+    if depth > 1:
+      get_quarters(base_dir + str(q) + '/',
+                   base_req + str(q),
+                   depth - 1)
+
+
+get_quarters(BASE_DIR, '', depth=9)
