1063ef88bd69db192fed17a9078469d5bd0b0e3c
Harald Schafer <harald.the.engineer@gmail.com>
2020-04-16 22:22:36 -0700

    new way of data management

--- download_data.py
+++ download_data.py
@@ -1,51 +1,31 @@
 #!/usr/bin/env python
 import requests
-import json
-import random
-from tqdm import trange
 import os.path
-from os import path
-import sys
-
-if len(sys.argv) < 3:
-  raise RuntimeError(" specify base dir and zoom level")
-
-base_dir = sys.argv[1]
-zoom = int(sys.argv[2])
-if base_dir[-1] != '/':
-  base_dir += '/'
 
 
-with open('api_key', 'r') as file:
-  key = file.read().replace('\n', '')
+BASE_DIR = '/raid.dell1/world/'
+MAX_ZOOM = 10
 
 
-def save_img(latitude, longitude, zoom, save_path):
-  api_request = ('https://dev.virtualearth.net/REST/v1/Imagery/Metadata/Aerial/' +
-                  str(latitude) + ',' + str(longitude) + '?zoomLevel=' + str(zoom) +
-                  '&key=' + key)
-  r =requests.get(api_request)
-  dic = json.loads(r.text)
-  print(dic)
-  image_url = dic['resourceSets'][0]['resources'][0]['imageUrl']
+def save_img(req, save_path):
+  image_url = 'http://ecn.t1.tiles.virtualearth.net/tiles/a' + req + '.jpeg?g=8384'
 
   receive = requests.get(image_url)
   with open(save_path,'wb') as f:
     f.write(receive.content)
 
 
-directory = base_dir + '/zoom_' + str(zoom) + '/'
-try:
-  os.mkdir(directory)
-except FileExistsError as e:
-  pass
 
-for i in trange(max(8192, 10*(4**zoom))):
-  file_name = directory + str(i)
-  if path.exists(file_name + '.txt'):
-    continue
-  lat = (180*random.random() - 90)
-  lon = (360*random.random() - 180)
-  with open(file_name + '.txt','w') as f:
-    f.write(str(lat) + ', ' + str(lon) + ', ' + str(zoom))
-  save_img(lat, lon, zoom, file_name + '.jpeg')
+def get_quarters(base_dir, base_req, depth):
+  for q in range(4):
+    if not os.path.isdir(base_dir + str(q)):
+      os.mkdir(base_dir + str(q))
+      save_img(base_req + str(q), base_dir + str(q) + '/img.jpeg')
+    print(base_req)
+    if depth > 1:
+      get_quarters(base_dir + str(q) + '/',
+                   base_req + str(q),
+                   depth - 1)
+
+
+get_quarters(BASE_DIR, '', depth=9)
--- training/dataloader.py
+++ training/dataloader.py
@@ -16,30 +16,91 @@ from torch.utils.data import Dataset, DataLoader
 from skimage import io
 
 
+TILE_SIZE = 256
+BASE_DIR = '/raid.dell1/world/'
+
+
 def pil_loader(path):
   # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
   with open(path, 'rb') as f:
     img = Image.open(f)
-    return img.convert('RGB')
+    return np.array(img.convert('RGB'))
+
+
+def xy_from_geodetic(lat, lon):
+  lat = np.clip(lat, -85.05, 85.05)
+  lon = np.clip(lon, -179.9999999, 179.9999999)
+  x = lon/360.0 + .5
+  sinlat = np.sin(np.radians(lat))
+  y = 0.5 - np.log((1 + sinlat) / (1 - sinlat)) / (4 * np.pi)
+  return x,y
+
+
+def get_tile_idx(x, y, zoom):
+  if (not 0 <= x < 1.0) or ((not 0 <= y < 1.0)):
+    return 'NULL'
+  if zoom == 0:
+    return ''
+  if x >= .5 and y >= .5:
+    q = '3'
+  elif y >= .5:
+    q = '2'
+  elif x >= .5:
+    q = '1'
+  else:
+    q = '0'
+  return q + get_tile_idx(x*2 % 1, y*2 % 1, zoom - 1)
+
+
+def get_tile_pos(x, y, zoom):
+  print(x,y)
+  if zoom == 0:
+    return x, y
+  return get_tile_pos(x*2 % 2, y*2 % 2, zoom - 1)
+
+
+def get_tile(idx):
+  if idx == 'NULL':
+    return np.zeros((TILE_SIZE, TILE_SIZE, 3), dtype=np.uint8)
+  sub_path = ''.join([a + '/' for a in idx])
+  path = BASE_DIR + sub_path + 'img.jpeg'
+  return pil_loader(path)
+ 
+
+def get_custom_tile(x, y, zoom):
+  assert 0 <= x < 1
+  assert 0 <= y < 1
+  assert zoom >= 1
+  zoom = int(zoom)
+  tiles = []
+  delta = (.5)**(zoom + 1)
+  for x_off in [-delta, delta]:
+    tiles.append([])
+    for y_off in [-delta, delta]:
+      tiles[-1].append(get_tile(get_tile_idx(x + x_off, y + y_off, zoom)))
+    tiles[-1] = np.vstack(tiles[-1])
+  tiles = np.hstack(tiles)
+  #x_pos, y_pos = get_tile_pos(x,y,zoom)
+  for i in range(zoom):
+    x = (x - .25) * 2 % 1.0 + .5
+    y = (y - .25) * 2 % 1.0 + .5
+  x_pix = int((x * TILE_SIZE))
+  y_pix = int((y * TILE_SIZE))
+  return tiles[y_pix - TILE_SIZE//2: y_pix + TILE_SIZE//2,
+               x_pix - TILE_SIZE//2: x_pix + TILE_SIZE//2]
+
+
+def get_custom_tile_geodetic(lat, lon, zoom):
+  x,y = xy_from_geodetic(lat, lon)
+  return get_custom_tile(x, y, zoom)
 
 
 class SatImageDataset(Dataset):
-  def __init__(self, root='/raid.dell2/world', transform=None):
-    self.root_dir = root
+  def __init__(self, transform=None, size=100000, max_zoom=8):
+    self.size = size
     self.transform = transform
-    zooms = [d for d in os.listdir(self.root_dir) if os.path.isdir(os.path.join(self.root_dir, d))]
-    self.samples = []
-    # TODO do all zooms
-    for zoom in zooms:
-      zoom_path = os.path.join(self.root_dir, zoom)
-      for file_name in tqdm(os.listdir(zoom_path)):
-        if file_name[-4:] == '.txt':
-          meta_name = file_name
-          img_name = file_name[:-4] + '.jpeg'
-          meta_path = os.path.join(zoom_path, meta_name)
-          img_path = os.path.join(zoom_path, img_name)
-          if os.path.isfile(img_path):
-            self.samples.append([img_path, meta_path])
+    self.samples = np.random.uniform(0.0, 1.0, size=(self.size, 3))
+    self.samples[:,2] = np.floor(max_zoom*self.samples[:,2]) + 1.0
 
   def __len__(self):
     return len(self.samples)
@@ -47,15 +108,12 @@ class SatImageDataset(Dataset):
   def __getitem__(self, idx):
     if torch.is_tensor(idx):
       idx = idx.tolist()
+    sample = self.samples[idx]
 
-    image = pil_loader(self.samples[idx][0])
-    with open(self.samples[idx][1]) as f:
-      meta = f.read().split(',')
-      meta = torch.FloatTensor([float(x) for x in meta])
-
+    image = Image.fromarray(get_custom_tile(*sample))
     if self.transform:
       image = self.transform(image)
-      image = image.mul(2).add(-1)
+    meta = torch.FloatTensor(sample)
 
     sample = {'image': image, 'meta': meta}
 
@@ -76,11 +134,10 @@ class dataloader:
     self.batchsize = int(self.batch_table[pow(2,resl)])
     self.imsize = int(pow(2,resl))
     self.dataset = SatImageDataset(
-                    root=self.root,
-                    transform=transforms.Compose(   [
-                                                    transforms.Resize(size=(self.imsize,self.imsize), interpolation=Image.NEAREST),
+                    transform=transforms.Compose([
+                                                 transforms.Resize(size=(self.imsize,self.imsize), interpolation=Image.NEAREST),
                                                     transforms.ToTensor(),
-                                                    ]))
+                                                 ]))
 
     self.dataloader = DataLoader(
             dataset=self.dataset,
